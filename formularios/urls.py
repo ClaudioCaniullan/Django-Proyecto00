59d21af20269db7253a248b82ccb4b651fac9fde
Maguilera85 <drianketing@gmail.com>
2021-01-02 12:35:33 -0300

    solucionando errores

--- formularios/urls.py
+++ formularios/urls.py
@@ -7,6 +7,5 @@ app_name = "formularios"
 urlpatterns = [
     path('tabla_examenes', views.tabla_examenes),
     path('tabla_creada', views.tabla_creada),
-    # path('crear_guitarra_manual', views.crear_guitarra_manual),
     path('<id>/borrar', views.eliminar_examen),
 ]
