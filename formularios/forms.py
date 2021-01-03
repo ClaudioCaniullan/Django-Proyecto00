49cf866dafe1b62b892cfe2a4edfa75326d620b0
Maguilera85 <drianketing@gmail.com>
2021-01-03 15:40:42 -0300

    avance en creacion de formularios

--- formularios/forms.py
+++ formularios/forms.py
@@ -3,26 +3,82 @@ from django.core import validators
 from django.core.exceptions import ValidationError
 
 
-class DiagnosticoExamen(forms.Form):
-    '''
-    hemograma
-    '''
-    list_exam = (
-                    ('Hemograma', 'Hemograma'), ('Perfil Bioquimico', 'Perfil Bioquimico'),
-                    ('Perfil Lipidico', 'Perfil Lipidico'),
-                    ('Orina', 'Orina'), ('Coagulación', 'Coagulación'),
-                    ('Glicemia', 'Glicemia'), ('Electrocardiograma', 'Electrocardiograma')
+
+    # list_exam = (
+    #                 ('Hemograma', 'Hemograma'), ('Perfil Bioquimico', 'Perfil Bioquimico'),
+    #                 ('Perfil Lipidico', 'Perfil Lipidico'),
+    #                 ('Orina', 'Orina'), ('Coagulación', 'Coagulación'),
+    #                 ('Glicemia', 'Glicemia'), ('Electrocardiograma', 'Electrocardiograma')
+    #             )
+    # select_exam = forms.ChoiceField(label='Tipo examen', choices=list_exam)
+
+class Hemograma(forms.Form):
+        ''' Formulario Hemograma '''
+    fecha_ingreso       = forms.DateField()
+    eritrocito          = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
+    leucocitos          = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
+    hemoglobina         = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
+    hematocrito         = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
+    vcm                 = forms.DecimalField(label='V.C.M.', max_digits=3, decimal_places=1, required=True)
+    chcm                = forms.DecimalField(label='C.H.C.M.', max_digits=3, decimal_places=1, required=True)
+    plaquetas           = forms.DecimalField(max_digits=4, decimal_places=1, required=True)
+    diagnostico         = forms.CharField(required=True)
+
+
+class PerfilBioquimico(forms.Form):
+    ''' Formulario Perfil Bioquimico '''
+    fecha_ingreso       = forms.DateField()
+    glucosa             = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
+    nitrogeno_ureico    = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
+    calcio              = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
+    fosforo             = forms.DecimalField(max_digits=2, decimal_places=1, required=True)
+    proteinas_totales   = forms.DecimalField(max_digits=2, decimal_places=1, required=True)
+    albumina            = forms.DecimalField(max_digits=2, decimal_places=1, required=True)
+    colesterol_total    = forms.DecimalField(max_digits=3 decimal_places=1, required=True)
+    acido_urico         = forms.DecimalField(max_digits=2, decimal_places=1, required=True)
+    fosfatas_alcalinas  = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
+    bilirrubina_total   = forms.DecimalField(max_digits=1, decimal_places=1, required=True)
+    ldh                 = forms.DecimalField(max_digits=3, decimal_places=1, required=True, label='LDH')
+    got_ast             = forms.DecimalField(max_digits=2, decimal_places=1, required=True, label='GOT/AST')
+    creatina            = forms.DecimalField(max_digits=1, decimal_places=1, required=True)
+    diagnostico         = forms.CharField(required=True)
+
+
+class PerfilLipidico(forms.Form):
+    ''' Formulario Perfil Lipidico '''
+    fecha_ingreso       = forms.DateField()
+    colesterol_total    = forms.IntegerField(MinValueValidator=1,  MaxValueValidator=200, required=True)
+    ldl                 = forms.IntegerField(MinValueValidator=1,  MaxValueValidator=100, required=True, label='Colesterol LDL')
+    hdl                 = forms.IntegerField(MinValueValidator=40,  MaxValueValidator=60, required=True, label='Colesterol HDL')
+    vldl                = forms.IntegerField(MinValueValidator=2,  MaxValueValidator=30, required=True, label='Colesterol VLDL')
+    trigleceridos       = forms.IntegerField(MinValueValidator=1,  MaxValueValidator=150, required=True)
+    diagnostico         = forms.CharField(required=True)
+
+class Orina(forms.Form):
+    ''' Formulario Orina'''
+    estado =    (
+                ('Positivo': 'Positivo'), 
+                ('Negativo': 'Negativo')
                 )
 
-    
-    select_exam = forms.ChoiceField(label='Tipo examen', choices=list_exam)
+    fecha_ingreso       = forms.DateField()
+    color               = forms.CharField()
+    densidad            = forms.DecimalField()
+    ph                  = forms.DecimalField()
+    glucosa             = forms.IntegerField(MinValueValidator=1, MaxValueValidator=20, required=True)
+    cetonas             = forms.ChoiceField(choices=(estado))
+    Urobilinogeno       = forms.DecimalField(MaxValueValidator=1, max_digits=1, decimal_places=1, required=True)
+    bilirrubina         = forms.ChoiceField(choices=(estado))
+    diagnostico         = forms.CharField(required=True)
 
-    fecha_ingreso = forms.DateField()
-    eritrocito = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
-    leucocitos = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
-    hemoglobina = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
-    hematocrito = forms.DecimalField(max_digits=3, decimal_places=1, required=True)
-    vcm = forms.DecimalField(label='V.C.M.', max_digits=3, decimal_places=1, required=True)
-    chcm = forms.DecimalField(label='C.H.C.M.', max_digits=3, decimal_places=1, required=True)
-    plaquetas = forms.DecimalField(max_digits=4, decimal_places=1, required=True)
-    diagnostico = forms.CharField(required=True)
+class Coagulacion(forms.Form):
+    ''' Formulario Coagulacion '''
+    pass
+
+class Glicemia(forms.Form):
+    ''' Formulario Glicemia '''
+    pass
+
+class Electrocardiograma(forms.Form):
+    ''' Formulario Electrocardiograma '''
+    pass
