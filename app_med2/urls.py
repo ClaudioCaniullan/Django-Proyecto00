bc2c4acf0adeb5e004f7f6380d73fe2aecc84d05
issi1489 <issi1489@gmail.com>
2021-01-04 19:21:09 -0300

    Se añade campo rut a json, se actualiza la app_med2, falta añadir form a html admin

--- app_med2/urls.py
+++ app_med2/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("home/", views.home),
-    path("usuario/", views.usuario)
+    path("admin/", views.admin),
+    path("paciente/", views.paciente),
 ]
--- app_med2/views.py
+++ app_med2/views.py
@@ -1,48 +1,37 @@
 from django.shortcuts import render
 import json
 
+#Table
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
+
 def home(request):
 
     return render(request,'home.html')
 
-def usuario(request):
+def admin(request):
+
     #-------------- Lectura de datos del usuario del .json --------------
     # Generar copia de los datos de usuarios
-    with open('../proyecto_web/app_med/data/usuarios.json', 'r') as file:
+    with open('../proyecto_web/app_med2/data/usuarios.json', 'r') as file:
         usuarios=json.load(file)
     #Seleccionar el usuario requerido y extraer los datos asociados
     usuario=usuarios['9999999-9']
     nombre_us=usuario['nombre']+" "+usuario['apellido']
     edad_us=usuario['edad']
     direccion_us=usuario['direccion']
-
-    #datos a entregar al html
-    context_us={'nombre_us':nombre_us,'edad_us':edad_us, 'direccion_us':direccion_us}
-
-    return render(request,'usuario.html',context_us)
-
-def user(request):
-    
-    #usuario= POST if usuario ="admin": vista admin else: vista paciente
-
-    #-------------- Lectura de datos del usuario del .json --------------
-    # Generar copia de los datos de usuarios
-    with open('../proyecto_web/app_med/data/usuarios.json', 'r') as file:
-        usuarios=json.load(file)
-    #Seleccionar el usuario requerido y extraer los datos asociados
-    usuario=usuarios['9999999-9']
-    nombre_us=usuario['nombre']+" "+usuario['apellido']
-    edad_us=usuario['edad']
-    direccion_us=usuario['direccion']
-
-    
 
     #-------------- Lectura de datos de pacientes del .json --------------
     # Generar copia de los datos de pacientes
-    with open('../proyecto_web/app_med/data/usuarios.json', 'r') as file:
+    with open('../proyecto_web/app_med2/data/usuarios.json', 'r') as file:
         pacientes=json.load(file)
 
-    #datos a entregar al html
-    context_us={'nombre_us':nombre_us,'edad_us':edad_us, 'direccion_us':direccion_us,'pacientes':pacientes}
+    lpacientes=pacientes
 
-    return render(request,'app_med/user.html',context_us)
+    #datos a entregar al html
+    context_us={'nombre_us':nombre_us,'edad_us':edad_us, 'direccion_us':direccion_us,'lpacientes':lpacientes}
+
+    return render(request,'admin.html',context_us)
+
+def paciente(request):
+    return render(request,'paciente.html')
